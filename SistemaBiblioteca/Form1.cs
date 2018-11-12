5290e358000df54e9bc2fb35d06ca043b356e2fe
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-11-12 19:17:51 -0400

    comenzando proyeto

--- SistemaBiblioteca/Form1.cs
+++ SistemaBiblioteca/Form1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+/// <summary>
+/// /https://github.com/ingMendez/Sistema-Biblioteca.git
+/// </summary>
 namespace SistemaBiblioteca
 {
     public partial class Form1 : Form
