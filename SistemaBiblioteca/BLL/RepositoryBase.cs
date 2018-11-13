518530401cffeb3cc27ea5e31637a321528185dd
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-11-13 08:07:57 -0400

    Avanzando

--- SistemaBiblioteca/BLL/RepositoryBase.cs
+++ SistemaBiblioteca/BLL/RepositoryBase.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace SistemaBiblioteca.BLL
@@ -104,5 +105,6 @@ namespace SistemaBiblioteca.BLL
             _contexto.Dispose();
         }
 
+       
     }
 }
